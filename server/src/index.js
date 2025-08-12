60d39a3978abcbde187620168be2e512eb98088d
merayes <120264154+merayes@users.noreply.github.com>
2025-08-12 16:39:15 +0300

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 const io = new SocketIOServer(server, {
   cors: {
-    origin: 'https://w-chat-app.vercel.app/',
+    origin: 'https://w-chat-app.vercel.app',
     methods: ['GET', 'POST']
   }
 });
